2b7046ccd3e6926208de0868f4087070ab7e821f
arshia <chillguyfromspace@gmail.com>
2025-07-13 21:02:55 +0330

    make a product model for connecting database

---
+++ src/main/java/com/example/demo/products/Product.java
@@ -0,0 +1,25 @@
+package com.example.demo.products;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id ;
+
+    String name;
+    String description;
+    Integer price;
+    String category;
+}
