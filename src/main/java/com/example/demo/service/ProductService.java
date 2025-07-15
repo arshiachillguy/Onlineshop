390c215a5e073c1768b9a9e304905e5aeb7013fe
arshia <chillguyfromspace@gmail.com>
2025-07-15 13:37:11 +0330

    make service package and class

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+import com.example.demo.products.Product;
+import com.example.demo.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ProductService {
+    @Autowired
+    public ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+
+        return productRepository.findAll();
+    }
+
+    public Product saveProducts(Product product){
+
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> getProductByID(Long id){
+
+        return productRepository.findById(id) ;
+    }
+
+    public void deleteProductByID(Long id){
+        productRepository.deleteById(id);
+
+    }
+
+
+
+}
