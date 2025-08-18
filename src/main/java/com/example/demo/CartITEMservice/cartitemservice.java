664738935b601df25987bcf9466cf035e98f7cc4
arshia <chillguyfromspace@gmail.com>
2025-08-18 16:32:28 +0330

    create service class for order basket

---
+++ src/main/java/com/example/demo/CartITEMservice/cartitemservice.java
@@ -0,0 +1,4 @@
+package com.example.demo.CartITEMservice;
+
+public class cartitemservice {
+}
---
+++ src/main/java/com/example/demo/ShoppingcartService/shoppingcartservice.java
@@ -0,0 +1,4 @@
+package com.example.demo.ShoppingcartService;
+
+public class shoppingcartservice {
+}
