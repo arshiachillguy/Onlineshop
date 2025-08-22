84906c88279b2225680a0e633221af49da08b951
arshia <chillguyfromspace@gmail.com>
2025-08-22 16:29:37 +0330

    create shopping cart service and add some lines

--- src/main/java/com/example/demo/ShoppingcartService/shoppingcartservice.java
+++ src/main/java/com/example/demo/ShoppingcartService/shoppingcartservice.java
@@ -1,4 +1,33 @@
 package com.example.demo.ShoppingcartService;
 
+import com.example.demo.ShoppingcartRepository.shoppingcartRepository;
+import com.example.demo.cartITEMRepository.cartitemrepository;
+import com.example.demo.products.Product;
+import com.example.demo.serviceforproducts.ProductService;
+import jakarta.transaction.Transactional;
+import org.apache.el.lang.ELArithmetic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Transactional
 public class shoppingcartservice {
+    @Autowired
+    private shoppingcartRepository shoppingcartRepository;
+
+    @Autowired
+    private cartitemrepository cartitemrepository;
+
+    @Autowired
+    private ProductService productService;
+
+    // add new product to order basket
+    public void addITEM(Long cartID , long productId , int quantity){
+       //check this product to understand it is exits or not
+        Optional<Product> product = Optional.ofNullable(productService.getProductByID(productId).orElseThrow(() -> new RuntimeException("not found this product")));
+    }
+
+
 }
