2a8f34e38359cbe73f3b27584ab96e1eea1d3248
arshia <chillguyfromspace@gmail.com>
2025-08-31 16:08:32 +0330

    edit cartitemservice and add some new methods

--- src/main/java/com/example/demo/CartITEMservice/cartitemservice.java
+++ src/main/java/com/example/demo/CartITEMservice/cartitemservice.java
@@ -6,10 +6,13 @@ import com.example.demo.cartRepository.cartRepository;
 import com.example.demo.cartitem.CartITEM;
 import com.example.demo.products.Product;
 import com.example.demo.serviceforproducts.ProductService;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.naming.InsufficientResourcesException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class cartitemservice {
@@ -44,7 +47,72 @@ public class cartitemservice {
 
 
         return cartitemrepository.save(item);
+    }
+
+    //edit order list
+    public CartITEM EditITEM(Long cartitemId , int newquantity ) throws InsufficientResourcesException {
+
+        if (newquantity < 0 ) {
+            throw new InsufficientResourcesException("quantity should be greater than zero !");
         }
+
+        if (newquantity == 0){
+            throw new InsufficientResourcesException("quantity most grater than zero");
+        }
+
+        CartITEM item = cartitemrepository.findById(cartitemId).orElseThrow(()-> new RuntimeException("cart item not found "));
+        Product product = item.getProduct();
+
+        // check balance
+        int mainQuantity =   newquantity - item.getQuantity();
+        if ( mainQuantity > 0 && product.getStock() < mainQuantity){
+            throw new InsufficientResourcesException("Insufficient stock for product ID: \"" + product.getId());
+        }
+
+        item.setQuantity(newquantity);
+        return cartitemrepository.save(item);
+
+    }
+
+    public void DeleteITEM(Long cartitemId){
+        CartITEM item1 = cartitemrepository.findById(cartitemId).orElseThrow(()->new RuntimeException("Cart item not found for ID: " + cartitemId));
+        Product product = item1.getProduct();
+        if (product == null){
+            throw new RuntimeException("Product not found for cart item ID: \"" + cartitemId);
+        }
+        int quantity = item1.getQuantity();
+        product.setStock(product.getStock()+ quantity);
+        productService.saveProducts(product);
+
+        cartitemrepository.delete(item1);
+    }
+
+    public List<CartITEM> getItemsByCartId(Long cartId){
+
+        if (cartId == null ){
+            throw new RuntimeException("Cart ID cannot be null  " + cartId);
+        }
+        cartRepository.findById(cartId).orElseThrow(()-> new RuntimeException("cart not found for id :" + cartId));
+        List<CartITEM> item1 = cartitemrepository.findByCartId(cartId);
+
+        return item1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
