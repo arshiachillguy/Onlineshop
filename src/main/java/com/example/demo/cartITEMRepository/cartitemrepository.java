1266a2af3caf2053d76de4ef4c5fa78da98bc71b
arshia <chillguyfromspace@gmail.com>
2025-08-31 16:07:56 +0330

    edit cartitemrepository

--- src/main/java/com/example/demo/cartITEMRepository/cartitemrepository.java
+++ src/main/java/com/example/demo/cartITEMRepository/cartitemrepository.java
@@ -3,5 +3,8 @@ package com.example.demo.cartITEMRepository;
 import com.example.demo.cartitem.CartITEM;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface cartitemrepository extends JpaRepository<CartITEM , Long> {
+    List<CartITEM> findByCartId(Long cartId);
 }
